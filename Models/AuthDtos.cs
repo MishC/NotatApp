ef9a1152eb7d9c8c3a55e03a6a0b93c30470ff66
MishC <michaela.chovancova@bergen.kommune.no>
2025-11-17 08:53:05 +0100

    dto from client

--- Models/AuthDtos.cs
+++ Models/AuthDtos.cs
@@ -1,5 +1,6 @@
 namespace NotatApp.Models;
-
+//DTO- data transfer object
+//What will come from frontend
 public record RegisterDto(string Email, string Password, string? PhoneNumber);
 public record LoginDto(string Email, string Password, string Channel);
 public record Verify2FaDto(string FlowId, string Code, string Channel);
