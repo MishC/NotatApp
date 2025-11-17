94424cf4633499926671436e8b09a65b16b58cd4
MishC <michaela.chovancova@bergen.kommune.no>
2025-11-17 11:39:47 +0100

    different written name space

--- Models/AuthDtos.cs
+++ Models/AuthDtos.cs
@@ -1,6 +1,7 @@
-namespace NotatApp.Models;
+namespace NotatApp.Models{
 //DTO- data transfer object
 //What will come from frontend
 public record RegisterDto(string Email, string Password, string? PhoneNumber);
 public record LoginDto(string Email, string Password, string Channel);
 public record Verify2FaDto(string FlowId, string Code, string Channel);
+};
