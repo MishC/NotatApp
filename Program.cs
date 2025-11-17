842f4cb60a8f713b0c8515e0f351d8f018d2ba69
MishC <michaela.chovancova@bergen.kommune.no>
2025-11-17 08:57:32 +0100

    registration of all services and identity

--- Program.cs
+++ Program.cs
@@ -2,14 +2,24 @@ using Microsoft.EntityFrameworkCore;
 using NotatApp.Data;
 using NotatApp.Repositories;
 using NotatApp.Services;
+using NotatApp.Models;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
 using Serilog;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+//DB
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 //Frontend CORS header
 builder.Services.AddCors(options =>
 {
@@ -22,6 +32,47 @@ builder.Services.AddCors(options =>
               .AllowCredentials(); // Allow credentials (optional)
     });
 });
+//Microsoft ASP.NET Identity handler
+// ASP.NET Core Identity (uses ApplicationDbContext)
+builder.Services
+    .AddIdentityCore<User>(opt => {
+        opt.User.RequireUniqueEmail = true;
+        opt.Password.RequiredLength = 8;
+        opt.SignIn.RequireConfirmedEmail = false; // flip to true in prod
+        opt.Tokens.AuthenticatorTokenProvider = TokenOptions.DefaultAuthenticatorProvider;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+//Check Token validity
+
+var jwt = builder.Configuration.GetSection("Jwt");
+
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwt["Issuer"],
+        ValidAudience = jwt["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!)),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization();
+
+
+
+
 
 
 // Serilog
@@ -39,18 +90,26 @@ builder.Host.UseSerilog();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer(); //for Swagger only
 builder.Services.AddSwaggerGen();
-
+//End of Swagger config
 
 builder.Services.AddScoped<INoteRepository, NoteRepository>();
 builder.Services.AddScoped<INoteService, NoteService>();
 
 builder.Services.AddScoped<IFolderRepository, FolderRepository>();
 builder.Services.AddScoped<IFolderService, FolderService>();
+builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// external API
+builder.Services.AddSingleton<IEmailSender, SendGridEmailSender>();
+builder.Services.AddSingleton<ISmsSender, TwilioSmsSender>();
+
+
 
 builder.Services.AddControllers();
 
@@ -65,12 +124,15 @@ var app = builder.Build();
 // Enable CORS middleware
 app.UseCors("AllowReactApp");
 
-// Configure the HTTP request pipeline.
+// Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+
 app.UseExceptionHandler(o => { });  
 app.UseSerilogRequestLogging();
 //app.UseHttpsRedirection();
@@ -78,8 +140,8 @@ app.UseRouting();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
