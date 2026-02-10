eebfb7c29a12bc2a74ec190474514e9785fa02b4
MishC <michaela.chovancova@bergen.kommune.no>
2026-02-10 11:56:31 +0100

    update as userId is nnullable

--- Models/Folder.cs
+++ Models/Folder.cs
@@ -16,9 +16,7 @@ namespace NotatApp.Models
         [StringLength(20, MinimumLength = 2)]
         public string? Name { get; set; }
 
-        [Required]
-        [ForeignKey(nameof(User))]
-        public string UserId { get; set; } = default!;  //update: added userId, as user can make a new folder
+        public string? UserId { get; set; } = null;  //update: added userId, as user can make a new folder
         
         [ValidateNever]
         [JsonIgnore]   
