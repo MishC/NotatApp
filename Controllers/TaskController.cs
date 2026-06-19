44f7a2c0ea43d4a4277d820348b082c34f743575
MishC <michaela.chovancova@bergen.kommune.no>
2026-06-19 11:52:04 +0200

    namespace fixed

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NotatApp.Models;
-using NotatApp.Services;
+using NotatApp.Services.CalendarServices;
 using NotatApp.Extensions;
 
 namespace NotatApp.Controllers
